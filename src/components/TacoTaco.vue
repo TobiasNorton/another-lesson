8381c19817a3bed5c3cdf5ea5a17ab3a41fabbca
Tobias Norton <tnorton20@yahoo.com>
2019-01-23 09:26:53 -0500

    More practice for demo

---
+++ src/components/TacoTaco.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="taco-container">
+    <p v-for="(data, index) in tacos" :key="index">I have a {{ data.taco }} taco.</p>
+    <p v-if="tacos.length >= 4">I am wise</p>
+    <p else>I need more tacos.</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TacoTaco",
+  data() {
+    return {
+      tacos: [
+        { taco: "carnitas" },
+        { taco: "super sweet al pastor" },
+        { taco: "weird" }
+      ]
+    };
+  }
+};
+</script>
+
+
+<style>
+.taco-container {
+  background-color: hsl(185, 50%, 75%);
+  padding: 3rem;
+}
+</style>
